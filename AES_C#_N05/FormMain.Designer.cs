f09a5dd2a97c22d66806508802ea28b5af3d993d
Admin <Admin@ADMIN-PC>
2026-06-03 07:42:18 +0700

    Add project files.

---
+++ AES_C#_N05/FormMain.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace AES_C__N05
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerateKey = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtbKey = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.cboAESVersion = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTittle.Location = new System.Drawing.Point(229, 36);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(544, 38);
+            this.lblTittle.TabIndex = 0;
+            this.lblTittle.Text = "CHƯƠNG TRÌNH MÃ HÓA/GIẢI MÃ AES";
+            this.lblTittle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(357, 508);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 5;
+            // 
+            // btnGenerateKey
+            // 
+            this.btnGenerateKey.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnGenerateKey.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGenerateKey.Location = new System.Drawing.Point(55, 427);
+            this.btnGenerateKey.Name = "btnGenerateKey";
+            this.btnGenerateKey.Size = new System.Drawing.Size(167, 44);
+            this.btnGenerateKey.TabIndex = 6;
+            this.btnGenerateKey.Text = "Tạo Khóa Ngẫu Nhiên";
+            this.btnGenerateKey.UseVisualStyleBackColor = false;
+            this.btnGenerateKey.Click += new System.EventHandler(this.btnGenerateKey_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnEncrypt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEncrypt.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEncrypt.Location = new System.Drawing.Point(334, 546);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(167, 53);
+            this.btnEncrypt.TabIndex = 7;
+            this.btnEncrypt.Text = "Mã hóa";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnDecrypt.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDecrypt.Location = new System.Drawing.Point(541, 546);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(167, 53);
+            this.btnDecrypt.TabIndex = 8;
+            this.btnDecrypt.Text = "Giải mã";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 594);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Kết Quả:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnSave.Location = new System.Drawing.Point(532, 787);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(167, 42);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Lưu kết quả";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnOpenFile.Location = new System.Drawing.Point(310, 787);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(167, 42);
+            this.btnOpenFile.TabIndex = 12;
+            this.btnOpenFile.Text = "Chọn file";
+            this.btnOpenFile.UseVisualStyleBackColor = false;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(55, 182);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(873, 113);
+            this.rtbInput.TabIndex = 13;
+            this.rtbInput.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "DỮ LIỆU ĐẦU VÀO";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClear.Location = new System.Drawing.Point(761, 436);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(167, 44);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Xóa dữ liệu";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rtbKey
+            // 
+            this.rtbKey.Location = new System.Drawing.Point(55, 353);
+            this.rtbKey.Name = "rtbKey";
+            this.rtbKey.Size = new System.Drawing.Size(873, 55);
+            this.rtbKey.TabIndex = 16;
+            this.rtbKey.Text = "";
+            this.rtbKey.TextChanged += new System.EventHandler(this.rtbKey_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 501);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "THAO TÁC";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(57, 630);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(871, 128);
+            this.rtbOutput.TabIndex = 18;
+            this.rtbOutput.Text = "";
+            // 
+            // cboAESVersion
+            // 
+            this.cboAESVersion.FormattingEnabled = true;
+            this.cboAESVersion.Location = new System.Drawing.Point(158, 306);
+            this.cboAESVersion.Name = "cboAESVersion";
+            this.cboAESVersion.Size = new System.Drawing.Size(121, 24);
+            this.cboAESVersion.TabIndex = 19;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(713, 851);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(215, 25);
+            this.lblStatus.TabIndex = 20;
+            this.lblStatus.Text = "Trạng thái: Sẵn sàng";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Văn Bản Gốc:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Khóa AES:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(1052, 923);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cboAESVersion);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rtbKey);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnGenerateKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTittle);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AES Encryption System";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTittle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerateKey;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtbKey;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.ComboBox cboAESVersion;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AES_C#_N05/FormMain.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AES_C__N05
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            cboAESVersion.Items.Add("AES-128");
+            cboAESVersion.Items.Add("AES-192");
+            cboAESVersion.Items.Add("AES-256");
+
+            cboAESVersion.SelectedIndex = 2;
+
+            lblStatus.Text = "Trạng thái: Sẵn sàng";
+        }
+
+        private void rtbKey_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int GetSelectedKeySize()
+        {
+            switch (cboAESVersion.Text)
+            {
+                case "AES-128":
+                    return 128;
+
+                case "AES-192":
+                    return 192;
+
+                default:
+                    return 256;
+            }
+        }
+
+        private void btnGenerateKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int keySize = GetSelectedKeySize();
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = keySize;
+                    aes.GenerateKey();
+
+                    rtbKey.Text = Convert.ToBase64String(aes.Key);
+                }
+
+                lblStatus.Text = "Trạng thái: Đã tạo khóa AES";
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Không thể tạo khóa AES.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(rtbInput.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng nhập dữ liệu cần mã hóa.",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(rtbKey.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng tạo hoặc nhập khóa AES.",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                string plainText = rtbInput.Text.Trim();
+                string keyString = rtbKey.Text.Trim();
+
+                byte[] key = Convert.FromBase64String(keyString);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.GenerateIV();
+
+                    byte[] iv = aes.IV;
+
+                    ICryptoTransform encryptor =
+                        aes.CreateEncryptor(aes.Key, aes.IV);
+
+                    byte[] plainBytes =
+                        Encoding.UTF8.GetBytes(plainText);
+
+                    byte[] encryptedBytes =
+                        encryptor.TransformFinalBlock(
+                            plainBytes,
+                            0,
+                            plainBytes.Length);
+
+                    byte[] result =
+                        new byte[iv.Length + encryptedBytes.Length];
+
+                    Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+
+                    Buffer.BlockCopy(
+                        encryptedBytes,
+                        0,
+                        result,
+                        iv.Length,
+                        encryptedBytes.Length);
+
+                    rtbOutput.Text =
+                        Convert.ToBase64String(result);
+                }
+
+                lblStatus.Text = "Trạng thái: Mã hóa thành công";
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(
+                    "Khóa AES không đúng định dạng.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Có lỗi xảy ra khi mã hóa dữ liệu.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(rtbInput.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng nhập dữ liệu cần giải mã.",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(rtbKey.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng nhập khóa AES.",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                byte[] fullCipher =
+                    Convert.FromBase64String(rtbInput.Text.Trim());
+
+                byte[] key =
+                    Convert.FromBase64String(rtbKey.Text.Trim());
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+
+                    byte[] iv = new byte[16];
+
+                    Array.Copy(fullCipher, 0, iv, 0, 16);
+
+                    aes.IV = iv;
+
+                    byte[] cipher =
+                        new byte[fullCipher.Length - 16];
+
+                    Array.Copy(
+                        fullCipher,
+                        16,
+                        cipher,
+                        0,
+                        cipher.Length);
+
+                    ICryptoTransform decryptor =
+                        aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    byte[] decryptedBytes =
+                        decryptor.TransformFinalBlock(
+                            cipher,
+                            0,
+                            cipher.Length);
+
+                    rtbOutput.Text =
+                        Encoding.UTF8.GetString(decryptedBytes);
+                }
+
+                lblStatus.Text = "Trạng thái: Giải mã thành công";
+
+     
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(
+                    "Dữ liệu mã hóa không đúng định dạng Base64.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (CryptographicException)
+            {
+                MessageBox.Show(
+                    "Khóa AES không chính xác hoặc dữ liệu đã bị thay đổi.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Không thể giải mã dữ liệu.",
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    rtbInput.Text = File.ReadAllText(ofd.FileName);
+
+                    lblStatus.Text = "Trạng thái: Đã mở file";
+                }
+                catch
+                {
+                    MessageBox.Show(
+                        "Không thể đọc file.",
+                        "Lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(rtbOutput.Text))
+            {
+                MessageBox.Show(
+                    "Không có dữ liệu để lưu.",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Text Files (*.txt)|*.txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(
+                        sfd.FileName,
+                        rtbOutput.Text);
+
+                    lblStatus.Text = "Trạng thái: Đã lưu file";
+                }
+                catch
+                {
+                    MessageBox.Show(
+                        "Không thể lưu file.",
+                        "Lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc muốn xóa toàn bộ dữ liệu?",
+                "Xác nhận",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                rtbInput.Clear();
+                rtbKey.Clear();
+                rtbOutput.Clear();
+
+                lblStatus.Text = "Trạng thái: Đã xóa dữ liệu";
+            }
+        }
+    }
+}
